a8735af1b4b8c45bc797df807fd6fa3387dafcfc
DOGLAS GABRIELL DOS SANTOS MACEDO <46077@fiecalunos.lan>
2026-09-04 16:21:23 -0300

    comments: Movendo código comentado para o final

--- src/Backend/app/Http/Controllers/ArtistaController.php
+++ src/Backend/app/Http/Controllers/ArtistaController.php
@@ -8,42 +8,6 @@ use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class ArtistaController extends Controller
 {
- /*
-    // =========================================================
-    // Metodo index, devolve a colecao inteira
-    // =========================================================
-
-    public function index() {
-        $colecaoArtistas = [
-            ['nome' => 'The Weeknd', 'estilo' => 'R&G / Pop'],
-            ['nome' => 'Daft Punk', 'estilo' => 'Eletrônica Clássica']
-        ];
-
-        // Envelope serializado
-        return response()->json([
-            'sucesso' => true,
-            'dados' => $colecaoArtistas
-        ]);
-
-        $artistas = Artist::all(); 
-        return response()->json($artistas, 200);
-    }
-
-
-    // =========================================================
-    // Metodo show: Devolve um item busado por 10
-    // =========================================================
-    public function show($id) {
-        return response()->json([
-            'sucesso' => true,
-            'mensagem' => "Log de busca ativado para o ID do artista" . $id
-        ]);
-
-    }
-
-*/
-
-
 
 // ==============================================================================
 // CREATE -> CRIAR
@@ -112,3 +76,37 @@ class ArtistaController extends Controller
     }
 }
 
+ /*
+    // =========================================================
+    // Metodo index, devolve a colecao inteira
+    // =========================================================
+
+    public function index() {
+        $colecaoArtistas = [
+            ['nome' => 'The Weeknd', 'estilo' => 'R&G / Pop'],
+            ['nome' => 'Daft Punk', 'estilo' => 'Eletrônica Clássica']
+        ];
+
+        // Envelope serializado
+        return response()->json([
+            'sucesso' => true,
+            'dados' => $colecaoArtistas
+        ]);
+
+        $artistas = Artist::all(); 
+        return response()->json($artistas, 200);
+    }
+
+
+    // =========================================================
+    // Metodo show: Devolve um item busado por 10
+    // =========================================================
+    public function show($id) {
+        return response()->json([
+            'sucesso' => true,
+            'mensagem' => "Log de busca ativado para o ID do artista" . $id
+        ]);
+
+    }
+
+*/
