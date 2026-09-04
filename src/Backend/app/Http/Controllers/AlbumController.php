e0f065d45e4fbf62a362bd3bb1e642125a6f41c8
DOGLAS GABRIELL DOS SANTOS MACEDO <46077@fiecalunos.lan>
2026-09-04 16:23:28 -0300

    comments: Comentando crud de albumController

--- src/Backend/app/Http/Controllers/AlbumController.php
+++ src/Backend/app/Http/Controllers/AlbumController.php
@@ -7,6 +7,68 @@ use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
+// ==============================================================================
+// CREATE -> CRIAR
+// ============================================================================== 
+    public function store(Request $request) {
+        $albums = Album::create($request->all());
+
+        return response()->json($albums);
+    }
+
+    
+
+// ==============================================================================
+// READ -> LER
+// ==============================================================================
+    public function index(Request $request) {
+        $allAlbuns = Album::all();
+
+        return response()->json($allAlbuns);
+    }
+
+
+    // Como irei buscar um album apenas na tabela, uso o parametro ID que sera passado
+    public function show($id) {
+
+        // Passo o parametro $id tambem por será este parametro que o método findOrFail buscará
+        $album = Album::findOrFail($id);
+
+        return response()->json($album);
+    }   
+
+
+
+// ==============================================================================
+// UPDATE -> ATUALIZAR UM DADO
+// ==============================================================================
+    public function update(Request $request, $id) {
+        $album = Album::findOrFail($id);
+
+        $album->update($request->all());
+
+        return response()->json([
+            "Mensagem" => 'Album atualizado com sucesso', 
+            "dados atualizado" => 'Ano de lançamento: ' . $album->release_year
+        ]);
+    }
+    
+
+// ==============================================================================
+// DELETE -> DELETAR UM DADO
+// ==============================================================================
+    public function destroy($id) {
+        $albumExcluido = Album::findOrFail($id);
+
+        $albumExcluido->delete();
+
+        return response()->json([
+            "Mensageem" => "Excluído com sucesso"
+        ]);
+    }
+}
+
+
     // public function index() {
     //     $albuns = [
     //         ['Álbum' => 'Wild West'],
@@ -23,48 +85,3 @@ class AlbumController extends Controller
     //         'mensagem' => 'Mensagem de interpole:' . $id
     //     ]);
     // }
-
-    public function store(Request $request) {
-        $albums = Album::create($request->all());
-
-        return response()->json($albums);
-    }
-
-    public function index(Request $request) {
-        $allAlbuns = Album::all();
-
-        return response()->json($allAlbuns);
-    }
-
-
-
-    // Como irei buscar um album apenas na tabela, uso o parametro ID que sera passado
-    public function show($id) {
-
-        // Passo o parametro $id tambem por será este parametro que o método findOrFail buscará
-        $album = Album::findOrFail($id);
-
-        return response()->json($album);
-    }   
-
-    public function update(Request $request, $id) {
-        $album = Album::findOrFail($id);
-
-        $album->update($request->all());
-
-        return response()->json([
-            "Mensagem" => 'Album atualizado com sucesso', 
-            "dados atualizado" => 'Ano de lançamento: ' . $album->release_year
-        ]);
-    }
-
-    public function destroy($id) {
-        $albumExcluido = Album::findOrFail($id);
-
-        $albumExcluido->delete();
-
-        return response()->json([
-            "Mensageem" => "Excluído com sucesso"
-        ]);
-    }
-}
