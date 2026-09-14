e67cb3b406010a78815b068952a5d02642b4bfd5
doglasgabriell <doglasgsmacedo@gmail.com>
2026-09-14 09:45:50 -0300

    style: useState notification and finishing playlistBar

--- src/Frontend/src/components/Header.jsx
+++ src/Frontend/src/components/Header.jsx
@@ -1,18 +1,65 @@
+import { useState } from "react";
+import { IoIosNotifications } from "react-icons/io";
+
 const Header = () => {
+  const [notification, setNotification] = useState(3);
+  const [aberto, setAberto] = useState(false);
+
+  function open() {
+    setAberto(!aberto);
+  }
+
+  function disabled() {
+    setNotification(0);
+    setAberto(!aberto);
+  }
+
   return (
-    <header className='sticky top-0 z-10 bg-zinc-900/90 p-4 flex items-center justify-between'>
+    <header className="sticky top-0 z-10 bg-zinc-900/90 p-4 flex items-center justify-between">
       <div className="flex gap-2">
         {/* Botões de Navegação */}
-        <button className='w-8 rounded-full bg-black/70 flex items-center justify-center text-zinc-400 cursor-not-allowed'>{'<'}</button>
-        <button className='w-8 rounded-full bg-black/70 flex items-center justify-center text-zinc-400 cursor-not-allowed'>{'>'}</button>
+        <button className="w-8 rounded-full bg-black/70 flex items-center justify-center text-zinc-400 cursor-not-allowed">
+          {"<"}
+        </button>
+        <button className="w-8 rounded-full bg-black/70 flex items-center justify-center text-zinc-400 cursor-not-allowed">
+          {">"}
+        </button>
       </div>
 
       <div className="flex gap-4 items-center">
-        <button className='text-zinc-400 font-bold hover:text-white hover:scale-105 transition-all'>Inscrever-se</button>
-        <button className='bg-white text-black font-bold px-6 py-2 rounded-full hover:scale-105 transition-transform'>Entrar</button>
+        <button className="text-zinc-400 font-bold hover:text-white hover:scale-105 transition-all">
+          Inscrever-se
+        </button>
+        <button className="bg-white text-black font-bold px-6 py-2 rounded-full hover:scale-105 transition-transform">
+          Entrar
+        </button>
+
+        <div className="flex items-center justify-center hover:bg-zinc-500/60 transition-colors rounded-full p-3 cursor-pointer relative">
+          <button onClick={open} className="relative cursor-pointer">
+            <div
+              className={`flex absolute h-6 w-6 bg-green-800 rounded-full left- bottom-5 items-center justify-center font-bold ${
+                notification > 0 ? "bg-red-800" : "bg-gren"
+              }`}
+            >
+              {notification}
+            </div>
+            <IoIosNotifications size={30} />
+          </button>
+
+          {aberto && (
+            <div className="w-25 h-15 rounded-md flex flex-col items-center justify-center absolute bg-zinc-200 text-zinc-100 font-bold top-12">
+              <button
+                onClick={disabled}
+                className=" bg-green-700 p-2 rounded-2xl cursor-pointer"
+              >
+                LIMPAR
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Frontend/src/components/Sidebar.jsx
+++ src/Frontend/src/components/Sidebar.jsx
@@ -1,38 +1,69 @@
-function Sidebar(){
+import playlistData from "../data/playlistAPI";
+import PlaylistAside from "./PlaylistAside";
+import PlaylistCard from "./PlaylistCard";
 
+function Sidebar() {
   const menuItems = [
     {
       id: 1,
       label: "Inicio",
-      iconClass: "https://www.svgrepo.com/show/535439/home-1.svg"
+      iconClass: "https://www.svgrepo.com/show/535439/home-1.svg",
     },
     {
       id: 2,
       label: "Buscar",
-      iconClass: "https://www.svgrepo.com/show/532555/search.svg"
-    }
+      iconClass: "https://www.svgrepo.com/show/532555/search.svg",
+    },
   ];
 
   return (
     <aside className="w-72 bg-black p-6 flex flex-col gap-6">
       <nav className="flex flex-col gap-4">
-        {
-          menuItems.map( (menu) => (
-            <div key={menu.id} className="flex gap-3">
-              <img className="w-6 h-6 invert" src={menu.iconClass} alt={menu.label} />
-              <a className="flex items-center gap-4 text-zinc-400 hover:text-white transition-colors font-semibold" href="#">{menu.label}</a>
-            </div>
-          ))
-        }
+        {menuItems.map((menu) => (
+          <div key={menu.id} className="flex gap-3">
+            <img
+              className="w-6 h-6 invert"
+              src={menu.iconClass}
+              alt={menu.label}
+            />
+            <a
+              className="flex items-center gap-4 text-zinc-400 hover:text-white transition-colors font-semibold"
+              href="#"
+            >
+              {menu.label}
+            </a>
+          </div>
+        ))}
       </nav>
 
       <div className="w-full h-px bg-zinc-800"></div>
 
-      <nav>
-        <a href="#" className="flex items-center gap-4 text-zinc-400 hover:text-white transition-colors font-semibold">
+      <nav className="flex flex-col gap-5 w-full flex-1">
+        <a
+          href="#"
+          className="flex items-center gap-4 text-zinc-400 hover:text-white transition-colors font-semibold"
+        >
           <div className="w-6 h-6 bg-zinc-300 rounded-sm"></div> Sua Biblioteca
         </a>
-      </nav>  
+
+        <div className="relative flex">
+          <button className="flex items-center gap-4 text-zinc-400 hover:text-white transition-colors font-semibold cursor-pointer">
+            <div className="w-6 h-6 bg-zinc-300 rounded-sm"></div> Minha playlist
+          </button>
+
+          <div className="absolute bg-zinc-700/60 w-full h-160 top-10 flex flex-col gap-2">
+              {
+                playlistData.map((p) => (
+                  <PlaylistAside 
+                    key={p.id}
+                    imagem={p.imagem}
+                    titulo={p.titulo}
+                  />
+                ))
+              }
+          </div>
+        </div>
+      </nav>
     </aside>
   );
 }
